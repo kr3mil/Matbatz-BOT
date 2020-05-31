c6204301fb497c5c5a0f95f3fd849f1acdb2e1c7
Kamil <theoriginalpole@gmail.com>
2020-05-31 09:51:23 +0100

    Added basic play and song queue

--- index.js
+++ index.js
@@ -1,26 +1,51 @@
 var unirest = require("unirest");
-var fs = require("fs");
 
 var req = unirest("GET", "https://deezerdevs-deezer.p.rapidapi.com/search");
 
+const ytdl = require("ytdl-core");
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const config = require('./config.json');
-const package = require('./package.json')
+
+var servers = {};
 
 bot.on('ready', () => {
     console.log('Online!');
 })
 
+function play(connection, message) {
+    var server = servers[message.guild.id];
+
+    server.dispatcher = connection.play(ytdl(server.queue[0], {filter: "audio"}));
+
+    server.queue.shift();
+
+    server.dispatcher.on("debug", function() {
+        console.log('debug');
+    });
+
+    server.dispatcher.on("finish", function() {
+        console.log('Songs left: ' + server.queue.length);
+        if(server.queue[0]){
+            play(connection, message);
+        }else{
+            connection.disconnect();
+        }
+    });
+}
+
 bot.on('message', message => {
+    if(message.content[0] !== config.Prefix) return;
     let args = message.content.substring(config.Prefix.length).split(" ");
 
     switch(args[0]){
         case 'search':
             if(!args[1]) return message.reply('Error, please enter a search term');
-            
+        
+            let searchTerm = args.slice(1).join(' ');
+
             req.query({
-                "q": args[1]
+                "q": searchTerm
             });
 
             req.headers({
@@ -33,19 +58,45 @@ bot.on('message', message => {
                 if (res.error) throw new Error(res.error);
 
                 let json = res.body;
-                let top = json['data'][1];
-                let msg = `!play ${top['artist']['name']} ${top['title_short']}`;
-                if(msg !== null){
-                    return message.channel.send(msg);
+                var msg = "```" + "Top 5 results:\n";
+                for(var i = 0; i < 5; i++){
+                    msg += `  ${json['data'][i+1]['artist']['name']} - ${json['data'][i+1]['title_short']}\n`;
                 }
+                msg += "```";
+                return message.channel.send(msg);
             });
-
             break;
-        case 'setPrefix':
-            if (message.member.hasPermission("ADMINISTRATOR")){
-                config.Prefix = args[1];
-                return message.channel.send('Changed prefix to: ' + config.Prefix);
+        case 'play':
+            if(!args[1]){
+                message.channel.send("You need to provide a link!");
+                return;
             }
+            //change here
+            if(!message.member.voice.channel){
+                message.channel.send("You must be in a channel to play the bot.");
+                return;
+            }
+            
+            //queues
+            if(!servers[message.guild.id]){
+                console.log('Adding server to servers array');
+                servers[message.guild.id] = {
+                queue: []
+            }}
+
+            var server = servers[message.guild.id];
+            console.log('Queue size: ' + server.queue.length);
+            server.queue.push(args[1]);
+            console.log('Song added to queue');
+            console.log('Queue size: ' + server.queue.length);
+            if(!message.guild.voice) message.member.voice.channel.join().then(function(connection){
+                console.log('Bot not in voice channel');
+                play(connection, message);
+            });
+            break;
+        case 'skip':
+            var server = servers[message.guild.id];
+            if(server.dispatcher) server.dispatcher.end();
             break;
         case 'help':
             break;
