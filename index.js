91c8fdfce4ea4c0238e94979a42f966671cf2295
Kamil <theoriginalpole@gmail.com>
2020-05-31 06:54:32 +0100

    Initial

---
+++ index.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const config = require('./config.json');
+const package = require('./package.json')
+
+const PREFIX = '!';
+
+const Http = new XMLHttpRequest();
+
+bot.on('ready', () => {
+    console.log('Online!');
+})
+
+bot.on('message', message => {
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+    console.log(args[0]);
+
+    switch(args[0]){
+        default:
+            break;
+    }
+})
+
+bot.login(config.Token);
